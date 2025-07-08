bc5c15fd53d5c77baef18449c8528470806f4a6b
Artem Strelnikov <piramida1237@gmail.com>
2025-07-08 18:15:31 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,10 @@
 function isEnoughCapacity(products, containerSize) {
   let sum = 0;
+  const values = Object.values(products);
+  for (const value of values) {
+    sum += value;
+  }
+  return sum <= containerSize ? true : false;
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
